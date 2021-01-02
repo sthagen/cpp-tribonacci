95ae6e67dd57d6f20a898242096822e9e391c8a0
Stefan Hagen <stefan@hagen.link>
2021-01-02 15:42:19 +0100

    Fix

--- include/tribonacci.hpp
+++ include/tribonacci.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 template <typename T> unsigned tribonacci(T n) {
-  return n == 0 ? 0 : n == 1 ? 1 : n == 2 ? tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+  return n == 0 ? 0 : n == 1 ? 1 : n == 2 ? 1 : tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
 }
 
 template <typename T> T tribonacci_iter(T i) {
